c7c897933ab2696bf7cdcb260264c4582d0127fb
SixArm <sixarm@sixarm.com>
2012-03-16 02:33:03 -0700

    Tune up organization

--- README.md
+++ README.md
@@ -7,7 +7,9 @@
 
 ## Introduction
 
-GeocodeAddress gem to connect to Google Maps API.
+GeocodeAddress uses the Google Maps API to geocode an address string to a JSON result.
+
+The address will cache as much as possible as it initializes and calls the Google Maps API.
 
 For docs go to <http://sixarm.com/sixarm_ruby_geocode_address/doc>
 
@@ -29,5 +31,13 @@ Require:
     require "sixarm_ruby_geocode_address"
 
 
+## Examples
 
+   address = GeocodeAddress.new("1 Main St, San Francisco, CA 94111")
+   address.href  #=> "http://maps.googleapis.com/maps/api/geocode/json?address=1+Main+St,+San+Francisco,+CA+94111&sensor=false"
+   address.get  #=> ...calls the Google Maps API via Net::HTTP and returns a JSON-formatted string of results
+   address.json  #=> ...calls the Google Maps API via Net::HTTP and returns a JSON parse of results
+   address.location  #=> {"lat" => 37.7931108, "lng" => -122.3964898}
+   address.lat  #=> 37.7931108
+   address.lng  #=> -122.3964898
 
--- lib/sixarm_ruby_geocode_address.rb
+++ lib/sixarm_ruby_geocode_address.rb
@@ -1,31 +1,6 @@
 # -*- coding: utf-8 -*-
-
 =begin rdoc
-
-= SixArm.com » Ruby » GeocodeAddress gem to connect to Google Maps API
-
-Author:: Joel Parker Henderson, joel@sixarm.com
-Copyright:: Copyright (c) 2011 Joel Parker Henderson
-License:: See LICENSE.txt file
-
-GeocodeAddress uses the Google Maps API to geocode an address string to a JSON result.
-
-The address will cache as much as possible as it initializes and calls the Google Maps API.
-
-Requires:
-  cgi
-  net/http
-  json
-
-@example
-   address = GeocodeAddress.new("1 Main St, San Francisco, CA 94111")
-   address.href => "http://maps.googleapis.com/maps/api/geocode/json?address=1+Main+St,+San+Francisco,+CA+94111&sensor=false"
-   address.get => ...calls the Google Maps API via Net::HTTP and returns a JSON-formatted string of results
-   address.json => ...calls the Google Maps API via Net::HTTP and returns a JSON parse of results
-   address.location => {"lat" => 37.7931108, "lng" => -122.3964898}
-   address.lat => 37.7931108
-   address.lng => -122.3964898
-
+Please see README
 =end
 
 require 'cgi'
